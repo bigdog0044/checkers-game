2359df7b2544b472e93d32bfafceed9ff58224c4
Jamie <jambo.peach@outlook.com>
2025-06-16 20:26:04 +0100

    Update .gitignore, pom.xml, and ServerThread.java; add DBUsernameAndPass class reference

--- server_code/ServerThread.java
+++ server_code/ServerThread.java
@@ -6,6 +6,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
 
 public class ServerThread implements  Runnable{
     private Socket socket; //socket that the client uses to connect
@@ -21,14 +24,17 @@ public class ServerThread implements  Runnable{
     @Override
     public void run(){
             try {
+                DBUsernameAndPass dbinfo = new DBUsernameAndPass();
                 String connectionID = serverMain.getConnectionID();
                 BufferedWriter output = new BufferedWriter( new OutputStreamWriter(socket.getOutputStream()));
                 BufferedReader incomingMSG = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
                 Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/checkersgamedb", dbinfo.getUsername(), dbinfo.getPassword());
                 
-            } catch (IOException | ClassNotFoundException e) {
-                System.out.println(e);
+            } catch (IOException | ClassNotFoundException | SQLException  e) {
+                System.out.println("Error on server thread: " + e);
             } finally{
                 if(socket != null){
                     try {
