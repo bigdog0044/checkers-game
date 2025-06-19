91438b13abb764dae2a61f029508b5f3363a6ee3
Jamie <jambo.peach@outlook.com>
2025-06-19 18:08:24 +0100

    developed the login system

--- server_code/ClientConnection.java
+++ server_code/ClientConnection.java
@@ -3,6 +3,7 @@ package server_code;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.Scanner;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
@@ -10,6 +11,9 @@ import java.net.Socket;
 public class ClientConnection {
     private static final String address = "127.0.0.1";
     private static final int port = 9000;
+    private static final Scanner keyboardOBJ = new Scanner(System.in);
+    private static String line;
+    private static String clientUUID;
     public static void main(String[] args){
         try {
             Socket socket = new Socket(address,port);
@@ -18,11 +22,73 @@ public class ClientConnection {
             BufferedReader incomingMSG = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             BufferedWriter output = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            // //outputting what the server initially sends
-            //System.out.println(incomingMSG.readLine());
-            // System.out.println(incomingMSG.readLine());
-            
 
+            line = incomingMSG.readLine();
+            boolean validAuth = false;
+
+            //loging portion
+            if (line.equals("AUTHREQ")){
+                
+                while(!(validAuth)){
+                    String username = "";
+                    String password = "";
+
+                    //does a infinite loop to constantly ask user for username and password
+                    while (username.equals("") || password.equals("")){
+                        System.out.print("Enter username: ");
+                        username = keyboardOBJ.nextLine();
+                        System.out.print("Enter password: ");
+                        password = keyboardOBJ.nextLine();
+                    }
+
+                    //sends out a request to the server to validate the username and password
+                    output.write("AUTHCHECK");
+                    output.newLine();
+                    output.write(username);
+                    output.newLine();
+                    output.write(password);
+                    output.newLine();
+                    output.flush();
+
+                    line = incomingMSG.readLine();
+
+                    if(line.equals("AUTHSUCCESS")){
+                        validAuth = true;
+                        while ((line = incomingMSG.readLine())!= null){
+                            clientUUID = line;
+                            System.out.println("Successful login");
+                        }
+                    } else if(line.equals("AUTHNOTSUCCESS")){
+                        System.out.println("No valid username and password found");
+                    }
+                    
+                }
+            }
+            System.out.println(clientUUID);
+            // while (line != null && !line.equals("CLOSE")){
+            //     System.out.println("test");
+            //     String username = "";
+            //     String password = "";
+            //     while (username.equals("") || password.equals("")){
+            //         System.out.print("Enter username: ");
+            //         username = keyboardOBJ.nextLine();
+            //         System.out.print("Enter password: ");
+            //         password = keyboardOBJ.nextLine();
+            //     }
+                
+            //     //sending authentication request to the server with username and password
+            //     output.write("AUTHREQ");
+            //     output.newLine();
+            //     output.write(username);
+            //     output.newLine();
+            //     output.write(password);
+            //     output.newLine();
+            //     output.flush();
+            // }
+
+            //closing connections
+            output.close();
+            incomingMSG.close();
             socket.close();
 
         } catch (IOException e) {
--- server_code/ServerThread.java
+++ server_code/ServerThread.java
@@ -5,10 +5,14 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Scanner;
 import java.net.Socket;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 
 public class ServerThread implements  Runnable{
     private Socket socket; //socket that the client uses to connect
@@ -24,16 +28,37 @@ public class ServerThread implements  Runnable{
     @Override
     public void run(){
             try {
-                DBUsernameAndPass dbinfo = new DBUsernameAndPass();
                 String connectionID = serverMain.getConnectionID();
                 BufferedWriter output = new BufferedWriter( new OutputStreamWriter(socket.getOutputStream()));
                 BufferedReader incomingMSG = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-
-                Class.forName("com.mysql.cj.jdbc.Driver");
-                Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/checkersgamedb", dbinfo.getUsername(), dbinfo.getPassword());
                 
-            } catch (IOException | ClassNotFoundException | SQLException  e) {
+                output.write("AUTHREQ");
+                output.newLine();
+                output.flush();
+                
+                
+                switch (incomingMSG.readLine()) {
+                    case "AUTHCHECK":
+                    String username = incomingMSG.readLine();
+                    String password = incomingMSG.readLine();
+                    
+                    String userUUID = validAuth(username, password);
+
+                    if (userUUID != null){
+                        output.write("AUTHSUCCESS");
+                        output.newLine();
+                        output.write(userUUID);
+                        output.flush();
+                    } else{
+                        output.write("AUTHNOTSUCCESS");
+                        output.flush();
+                    }
+                    break;
+                    
+                    default:
+                    break;
+                }
+            } catch (IOException e) {
                 System.out.println("Error on server thread: " + e);
             } finally{
                 if(socket != null){
@@ -46,6 +71,35 @@ public class ServerThread implements  Runnable{
                     }
                 }
             }
-
+            
         }
+        
+        public String validAuth(String username, String password){
+            try{
+                DBUsernameAndPass dbinfo = new DBUsernameAndPass();
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/checkersgamedb", dbinfo.getUsername(), dbinfo.getPassword());
+                
+                String sqlStatement = "select * from userinfo where username = ? and password = ?;";
+
+                //this is used below as not only is it the better as it has massive security improvement and also enables portability
+                PreparedStatement preparedSQL = connection.prepareStatement(sqlStatement);
+                preparedSQL.setString(1,username);
+                preparedSQL.setString(2,password);
+
+                
+                ResultSet resultSet = preparedSQL.executeQuery();
+
+                while(resultSet.next()){
+                    if(resultSet.getString("username").equals(username) && resultSet.getString("password").equals(password)){
+                        return resultSet.getString("ID");
+                    }
+                }
+
+            } catch (ClassNotFoundException | SQLException e){
+            System.out.println("Valid auth error: " + e);
+            }
+        return null;
+    }
 }
