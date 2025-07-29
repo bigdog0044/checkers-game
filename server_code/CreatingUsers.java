c6f3f318a35dde5a6295ee336ea6633cdf43de4b
Jamie <jambo.peach@outlook.com>
2025-07-29 14:56:24 +0100

    added files back as they were deleted by mistake

---
+++ server_code/CreatingUsers.java
@@ -0,0 +1,106 @@
+	
+
+	package server_code;
+
+import java.net.Socket;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.UUID;
+
+class CreatingUsersError extends Exception{
+    public CreatingUsersError(String msg){
+        super(msg);
+    }
+}
+
+public class CreatingUsers{
+	private static Connection connection;
+	private static BufferedWriter output;
+
+	public CreatingUsers(){
+		try{
+			DBUsernameAndPass dbinfo = new DBUsernameAndPass();
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			connection = DriverManager.getConnection(
+	        "jdbc:mysql://localhost:3306/checkersgamedb", dbinfo.getUsername(), dbinfo.getPassword());
+		} catch (ClassNotFoundException | SQLException e){
+			System.out.println("Error on CreatingUsers: " + e);
+		}
+	}
+
+	/*
+	* this method is used to create and insert users into the database
+	* @returns true if user has been added to the database 
+	*/
+	public boolean createUser(String username, String password, String role ) throws CreatingUsersError{
+		try{
+
+            if(checkUsername(username)){
+            	UUID userUUID = UUID.randomUUID();
+            	String sqlStatement = "INSERT INTO userinfo(`ID`,`username`,`password`,`gameSessionID`,`lastIpAdrrConn`,`lastDateConn`,`lastTimeOfConn`,`searchingForGame`,`isInGame`,`privilegeType`) VALUES (?,?,?,?,?,?,?,?,?,?);";
+            	PreparedStatement preparedSQL = connection.prepareStatement(sqlStatement);
+        		
+        		preparedSQL.setString(1,userUUID.toString());
+        		preparedSQL.setString(2,username);
+        		preparedSQL.setString(3,password);
+        		preparedSQL.setNull(4,java.sql.Types.VARCHAR);
+        		preparedSQL.setNull(5,java.sql.Types.VARCHAR);
+        		preparedSQL.setNull(6,java.sql.Types.DATE);
+        		preparedSQL.setNull(7,java.sql.Types.TIME);
+        		preparedSQL.setBoolean(8,false);
+        		preparedSQL.setBoolean(9,false);
+        		preparedSQL.setString(10,role);
+
+        		preparedSQL.executeUpdate();
+
+        		return true; //user created successfully
+            }
+
+            //throws an error if the same username is found in the database
+            throw new CreatingUsersError("Invalid username: Username exists in database. Please create a new user without using the same usernames");
+		} catch (SQLException e){
+			System.out.println("Error on CreatingUsers createUser method: " + e);
+		} finally{
+			try{
+				if(output != null){
+					output.close();
+				}	
+			} catch (IOException e){
+				/*does nothing*/
+			}
+		}
+
+		return false;
+	}
+
+	/*
+	* checks to see if username exists in the database
+	* @returns true if no username exists in database and false if it does exist
+	*/
+	private boolean checkUsername(String username){
+		try{
+			String sqlStatement = "SELECT * FROM `userinfo` WHERE username = ?;";
+
+	        PreparedStatement preparedSQL = connection.prepareStatement(sqlStatement);
+	        preparedSQL.setString(1,username);
+	        ResultSet resultSet = preparedSQL.executeQuery();
+
+	        if(!resultSet.next()){
+	                return true; //username is valid
+	        }
+		} catch (SQLException e){
+			System.out.println("Error on checkUsername method: " + e);
+		}
+        
+
+        return false;
+    }
+}
---
+++ server_code/SettingGamesUp.java
@@ -0,0 +1,162 @@
+package server_code;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.Socket;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileWriter;
+import java.util.UUID;
+import game_code.CheckerBoard;
+
+
+public class SettingGamesUp {
+	private static final String saveDir = "game-save-files/";
+	private static final String trackerFileName = "saveTracker";
+	private String sessionFolderUUID;
+	private String sessionFolderLocation;
+	private static BufferedWriter output;
+	private String userUUID;
+
+	public SettingGamesUp(String userUUID, Socket socket, String userID, CheckerBoard checkerBoardOBJ){
+		try{
+			output = new BufferedWriter( new OutputStreamWriter(socket.getOutputStream()));
+			this.userUUID = userID;
+
+			output.write("SESSIONCREATING");
+			output.newLine();
+			output.flush();
+
+			 File folderOBJ = new File(saveDir + userUUID);
+
+			 //creating user folder
+			 if(!folderOBJ.exists()){
+				 folderOBJ.mkdirs();
+
+				 SendingCreationMSG("folder", "creating");
+			 }
+
+
+			 //generates a new UUID for a session folder
+			 UUID gameFolderUUID = UUID.randomUUID();
+
+			 sessionFolderLocation = gameCreation(gameFolderUUID.toString(), userUUID);
+			 //letting user know that user session has been created
+			 if(sessionFolderLocation != null){
+			 	output.write("New game session has been created with the following ID: " + gameFolderUUID.toString());
+			 	output.newLine();
+			 	output.flush();
+
+			 	if (gameFileCreation(sessionFolderLocation, userUUID, checkerBoardOBJ)){
+			 		//add stuff in  
+			 	}
+			} else {
+				System.out.println("An error has occured when creating folder");
+			}
+			output.write("ENDSESSCREATE");
+			output.newLine();
+			output.flush();
+
+		} catch (IOException e){
+			System.out.println("Error on UserFolderSetup: " + e);
+		}
+	}
+
+
+	//returning current folder UUID so it can be used by other parts of the program
+	public String getCurrentFolderUUID(){
+		return sessionFolderUUID;
+	}
+
+
+	//used to send custom creation messages
+	private void SendingCreationMSG(String startingMSG,String message){
+		try{
+			output.write(startingMSG + " new " + message + "!");
+			output.newLine();
+			output.flush();
+		} catch (IOException e){
+			System.out.println("Error on sendingcreationmsg: " + e);
+		}
+
+	}
+
+
+	/*
+	* used to create the folder in which game files have been saved in
+	* @return directory location if successful folder creation
+	*/
+	private String gameCreation(String folderName, String userID){
+		File gameFolderOBJ = new File(saveDir + userID + "/"+ folderName);
+		gameFolderOBJ.mkdir();
+
+		return gameFolderOBJ.getPath() + "\\";
+	}
+
+
+
+	/*
+	* used to create and write into files all data required to get the game running
+	*@return true if successful
+	*/
+	private boolean gameFileCreation(String targetFolder,String userID, CheckerBoard board){
+		try{
+			//outputs to server for logs reason
+			System.out.println("Creating folder on following path: " + targetFolder);
+
+			//creating file objects
+			File metaDataOBJ = new File(targetFolder,"metadata.txt");
+			File gameSaveOBJ = new File(targetFolder,"gameSave.txt");
+			boolean successCreaton = false;
+
+			if (metaDataOBJ.createNewFile() && gameSaveOBJ.createNewFile()){
+			
+				try(
+					FileWriter metaDataWriterOBJ = new FileWriter(metaDataOBJ.getPath());
+					FileWriter gameSaveWriterOBJ = new FileWriter(gameSaveOBJ.getPath());
+				) {
+
+					//this does the writing for the meta data file
+					metaDataWriterOBJ.write("Owner: " + userID);
+					metaDataWriterOBJ.write("\n");
+					metaDataWriterOBJ.write("Game-save-dir-location: " + gameSaveOBJ.getPath());
+					metaDataWriterOBJ.write("\n");
+					metaDataWriterOBJ.write("Number-rows: " + board.returnRowLen());
+					metaDataWriterOBJ.write("\n");
+					metaDataWriterOBJ.write("Number-columns: " + board.returnColLen());
+					metaDataWriterOBJ.write("\n");
+					metaDataWriterOBJ.write("Max-players: 2");
+
+
+					//this handles writing the board peices to the file itself
+
+					for (int[] row : board.boardStatus()){
+						for (int colPos = 0; colPos < row.length; colPos++){
+
+							if(colPos == row.length - 1){
+								gameSaveWriterOBJ.write(row[colPos] + "\n");
+							} else {
+								gameSaveWriterOBJ.write(row[colPos] + " ");
+							}
+						}
+					}
+
+				} catch(IOException error){
+					System.out.println("gameFileCreation method error:" + error);
+
+				}
+
+			
+
+
+			}
+			return successCreaton;
+		} catch (IOException error){
+			System.out.println("Error on file creation: " + error);
+			return false;
+		}
+		
+		
+	}
+}
